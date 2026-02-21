056c3adb10b6fa8ac69521c4ad07722f9145f54b
Ivan Patiño <leopasua02@gmail.com>
2026-02-20 23:41:27 -0500

    chore: Remove obsolete API configuration files

--- app/src/main/java/com/example/safecity/network/ApiConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.example.safecity.network
-
-object ApiConfig {
-
-    const val BASE_URL = "http://192.168.1.50:3001"
-    // const val BASE_URL = "http://10.0.2.2:3001"
-    // Celular real (cambia por tu IP):
-    // const val BASE_URL = "http://192.168.1.50:3001"
-}
--- app/src/main/java/com/example/safecity/network/BackendConfig.kt
+++ app/src/main/java/com/example/safecity/network/BackendConfig.kt
@@ -4,5 +4,5 @@ object BackendConfig {
     const val BASE_URL = "http://10.0.2.2:3001/"
     //"http://10.0.2.2:3001/" "http://10.0.2.2:3001/"
     // Para celular real, cambiar por tu IP:
-    // const val BASE_URL = "http://192.168.10.7:3001/"
+    // const val BASE_URL = "http://192.168.10.9:3001/"
 }
