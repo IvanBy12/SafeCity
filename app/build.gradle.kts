007cbea2cb08a4dc6c02d6557280ed5e531773c0
Ivan Patiño <leopasua02@gmail.com>
2026-02-20 21:24:18 -0500

    Chore: Remove old session file and apply minor Gradle formatting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
+
 }
 
 android {
@@ -44,8 +45,11 @@ dependencies {
     implementation("com.google.firebase:firebase-messaging")
     implementation("com.google.firebase:firebase-storage") // NUEVO: Para subir fotos
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
+
     //test
     testImplementation("junit:junit:4.13.2")
+
+
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
