8e9143aac9f03958f83525efed26c4bf1a56eed5
IvanBy12 <leopasua02@gmail.com>
2026-04-09 23:18:30 -0500

    Fix: ignorar archivos innecesarios

--- app/src/main/java/com/example/safecity/network/BackendConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.example.safecity.network
-
-object BackendConfig {
-    const val BASE_URL = "http://10.0.2.2:3001/"
-    //"http://10.0.2.2:3001/" "http://10.0.2.2:3001/"
-    // Para celular real, cambiar por tu IP:
-    // const val BASE_URL = "http://192.168.10.9:3001/"
-}
